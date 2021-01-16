927999385f5259a4f62d7682791e3d9ceb5769e3
Michael Cohen <scudette@gmail.com>
2021-01-17 01:41:56 +1000

    Added FAT timestamp

--- enumeration.go
+++ enumeration.go
@@ -3,13 +3,14 @@ package vtypes
 import (
 	"fmt"
 	"io"
+	"strconv"
 
 	"github.com/Velocidex/ordereddict"
 	"www.velocidex.com/golang/vfilter"
 )
 
 type EnumerationParser struct {
-	choices *ordereddict.Dict
+	choices map[int64]string
 	parser  Parser
 }
 
@@ -28,18 +29,52 @@ func (self *EnumerationParser) New(profile *Profile, options *ordereddict.Dict) 
 		return nil, fmt.Errorf("Enumeration parser requires a type in the options: %w", err)
 	}
 
+	mapping := make(map[int64]string)
+
+	// Support 2 ways of providing the mapping - choices has ints
+	// as keys and map has strings as keys.
 	choices, pres := options.Get("choices")
-	if !pres {
-		choices = ordereddict.NewDict()
+	if pres {
+		choices_dict, ok := choices.(*ordereddict.Dict)
+		if !ok {
+			return nil, fmt.Errorf("Enumeration parser requires choices to be a mapping between numbers and strings")
+		}
+
+		for _, k := range choices_dict.Keys() {
+			v, _ := choices_dict.Get(k)
+			i, err := strconv.ParseInt(k, 0, 64)
+			if err != nil {
+				return nil, fmt.Errorf("Enumeration parser requires choices to be a mapping between numbers and strings (not %v)", k)
+			}
+
+			v_str, ok := v.(string)
+			if !ok {
+				return nil, fmt.Errorf("Enumeration parser requires choices to be a mapping between numbers and strings")
+			}
+
+			mapping[i] = v_str
+		}
 	}
 
-	choices_dict, ok := choices.(*ordereddict.Dict)
-	if !ok {
-		return nil, fmt.Errorf("Enumeration parser requires choices to be a mapping between numbers and strings")
+	choices, pres = options.Get("map")
+	if pres {
+		choices_dict, ok := choices.(*ordereddict.Dict)
+		if !ok {
+			return nil, fmt.Errorf("Enumeration parser requires map to be a mapping between strings and numbers")
+		}
+		for _, k := range choices_dict.Keys() {
+			v, _ := choices_dict.Get(k)
+			v_int, ok := to_int64(v)
+			if !ok {
+				return nil, fmt.Errorf("Enumeration parser requires map to be a mapping between strings and numbers")
+			}
+
+			mapping[v_int] = k
+		}
 	}
 
 	return &EnumerationParser{
-		choices: choices_dict,
+		choices: mapping,
 		parser:  parser,
 	}, nil
 }
@@ -47,10 +82,14 @@ func (self *EnumerationParser) New(profile *Profile, options *ordereddict.Dict) 
 func (self *EnumerationParser) Parse(
 	scope vfilter.Scope, reader io.ReaderAt, offset int64) interface{} {
 
-	value := fmt.Sprintf("%v", self.parser.Parse(scope, reader, offset))
-	string_value, pres := self.choices.Get(value)
+	value, ok := to_int64(self.parser.Parse(scope, reader, offset))
+	if !ok {
+		return vfilter.Null{}
+	}
+
+	string_value, pres := self.choices[value]
 	if !pres {
-		string_value = value
+		string_value = fmt.Sprintf("%#x", value)
 	}
 	return string_value
 }
--- timestamp.go
+++ timestamp.go
@@ -92,3 +92,39 @@ func (self *WinFileTime) Parse(
 
 	return time.Unix((value/self.factor/10000000)-11644473600, 0)
 }
+
+type FatTimestamp struct {
+	profile *Profile
+}
+
+func (self *FatTimestamp) New(profile *Profile, options *ordereddict.Dict) (Parser, error) {
+	return &FatTimestamp{profile: profile}, nil
+}
+
+func (self *FatTimestamp) Parse(
+	scope vfilter.Scope, reader io.ReaderAt, offset int64) interface{} {
+
+	parser, err := self.profile.GetParser("uint32", nil)
+	if err != nil {
+		return vfilter.Null{}
+	}
+
+	date_int, ok := to_int64(parser.Parse(scope, reader, offset))
+	if !ok {
+		return vfilter.Null{}
+	}
+
+	// Dos times are stored as 2 uint16 numbers - first the date
+	// then the time so swap them.
+	date_int = ((date_int & 0xFFFF) << 16) + (date_int >> 16)
+
+	year := 1980 + (date_int >> 25)
+	month := (date_int >> 21) & ((1 << 4) - 1)
+	day := (date_int >> 16) & ((1 << 6) - 1)
+	hour := (date_int >> 11) & ((1 << 6) - 1)
+	min := (date_int >> 5) & ((1 << 7) - 1)
+	sec := (date_int) & ((1 << 6) - 1)
+
+	return time.Date(int(year), time.Month(month), int(day),
+		int(hour), int(min), int(sec), 0, time.UTC)
+}
